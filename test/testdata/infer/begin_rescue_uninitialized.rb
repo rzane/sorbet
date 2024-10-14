281f05f93e6c9e13937fbaef325731853150481c
Jake Zimmerman <zimmerman.jake@gmail.com>
2024-10-14 13:58:02 -0700

    Add test whose behavior changes with this PR

---
+++ test/testdata/infer/begin_rescue_uninitialized.rb
@@ -0,0 +1,22 @@
+# typed: true
+extend T::Sig
+
+sig { returns(String) }
+def returns_string = ""
+
+sig { returns(T.nilable(String)) }
+def returns_nilable_string = nil
+
+begin
+  x = returns_string
+rescue ArgumentError
+  begin
+    x = returns_string
+  rescue ArgumentError
+    raise
+  end
+end
+
+T.reveal_type(x)
+
+x&.chars || []
